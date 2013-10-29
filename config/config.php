afff29e60bea50fb64d9b193d5e92fe08b611794
Mari <mari2daniel@yahoo.com>
2013-10-28 23:51:56 -0700

    Deleted files

--- config/config.php
+++ config/config.php
@@ -9,7 +9,7 @@ define("BASEURL", 'http://localhost/LooneyLimericks');
 define("SITENAME", "Looney Limericks");
 define("HOST", "localhost");
 define("USER", "root");
-define("PASSWORD", "");
+define("PASSWORD", "root");
 define("DBNAME", "looney_limericks_db");
 
 
